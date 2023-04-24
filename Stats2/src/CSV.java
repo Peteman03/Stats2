4b7911cbe8aa0281f7aba14cf504f4494b1c446e
Peteman03 <plw7203@outlook.com>
2023-04-24 13:08:28 -0400

    Finished csv reader and csv loader with comments

--- Stats2/src/CSV.java
+++ Stats2/src/CSV.java
@@ -4,35 +4,57 @@ import com.opencsv.CSVWriter;
 
 public class CSV 
 {
-	public void writetoFile(int start, int end, double incriment)
+	/**
+	 * The method the write data to a file using the OpenCSV library and its CSVWriter method
+	 * @param start the location that the user wants the graph to start
+	 * @param end the location on the graph where the user wants the graph to end
+	 * @param increment what the user wants the graph to increment by
+	 */
+	public void writetoFile(int start, int end, int increment)
 	{
+		//create a file with the name function.csv
 		File file = new File("function.csv");
 		
+		//try block
 		try
 		{
+			//create a filewriter object and tells it to write on the file that was created
 			FileWriter filewriter = new FileWriter(file);
 			
+			//using the CSVWriter method from the opencsv library to create a CSVWriter object with the writer as filewriter and the comma as the separator
 			CSVWriter writer = new CSVWriter(filewriter);
 			
+			//create the arraylist of string arrays to store the data
 			ArrayList<String[]> data = new ArrayList<String[]>();
 			
+			//add the heading line to the CSV fie
 			data.add(new String[] {"y=x^3+x^2", "x", "y"});
 			
-			for(double i = start; i <= end; i += incriment)
+			//for loop to loop through the graph sating at the start point and ending at the end point going up by the increment
+			for(int i = start; i <= end; i += increment)
 			{
-				Double variable =i;
-				Double answer = Math.pow(i, 3) + Math.pow(i, 2);
+				//set the x coordinate to variable looping through the array and set it to the wrapper class Integer
+				Integer variable = i;
+				//get the y value by adding x^3 and x^2 together and set it to the wrapper class Integer
+				Integer answer = (int)(Math.pow(i, 3) + Math.pow(i, 2));
+				//turn the answer into a string so it can be put in the array 
 				String y = answer.toString();
+				//turn the x coordinate into a string so it can be entered into the array
 				String x = variable.toString();
+				//add the new line of data to the array
 				data.add(new String[] {null, x, y});
 			}
 			
+			//write all the elements in the ArrayList data to the file
 			writer.writeAll(data);
+			//close the writer
 			writer.close();
 		}
 
+		//catch block to catch any IO exception errors
 		catch (IOException e) 
 		{
+				//print the error
 		        e.printStackTrace();
 		}
 	}
--- Stats2/src/CSVLoader.java
+++ Stats2/src/CSVLoader.java
@@ -1,64 +1,84 @@
 import java.util.*;
 import java.io.*;
 
-
+//This csv loader was provided by my data structures II professor last semester
+//I just altered the code to work for this project
 public class CSVLoader 
 {
 
 	public static void main(String[] args) 
 	{
+		//create three arraylist of type string to hold the data, one for the x value, one for the y value, and 1 for the header
 		ArrayList<String> x = new ArrayList<>();
 		ArrayList<String> y = new ArrayList<>();
 		ArrayList<String> header = new ArrayList<>();
 		
-		String filename = "function.csv";
+		//create a file with the filename function.csv
+		File file = new File("function.csv");
 		
-		File file = new File(filename);
-		
+		//create a scanner object
 		Scanner in = null;
 		
+		//try block to make sure the file exists
 		try 
 		{
+			//create a new scanner that pulls data from the file
 			in = new Scanner(file);
 			
+			//try block to make sure the file is readable
 			try
 			{
+				//check that the file has a line to read in
 				while(in.hasNextLine())
 				{
+					//create a string variable and set it to the line that is being read in
 					String line = in.nextLine();
 					
-					ArrayList<String> row = new ArrayList<>();
-					
+					//create an array of type string to hold the variable line that is split around commas
 					String[] data = line.split(",");
 					
+					//add the first element of the array to the header arraylist
 					header.add(data[0]);
+					//add the second element of the array to the x arraylist
 					x.add(data[1]);
+					//add the third element of the array to the y arraylist
 					y.add(data[2]);
 				
 				}
 			}
+			//catch statement for a no such element error
 			catch (NoSuchElementException e) 
 			{
+				//print statement for the error
 				System.err.println("Record Error: " + e.getMessage());
 			} 
+			//catch statement for the index out of bounds error
 			catch (IndexOutOfBoundsException e) 
 			{
+				//print statement for the error
 				System.err.println("Parse Error: " + e.getMessage());
 			} 
+			//catch statement for the number format exception error
 			catch (NumberFormatException e) 
 			{
+				//print statement for the error
 				System.err.println("Data Error: " + e.getMessage());
 			} 
 			finally 
 			{
+				//close the scanner object
 				in.close();
 			}
 			
 		} 
+		//catch block if the file is not found
 		catch (FileNotFoundException e) 
 		{
+			//Print statement for the error
 			System.err.println("File Unavailable: " + e.getMessage());
 		}
+		
+		//Print statements to print out all of the arrays with spaces between
 		System.out.print(header + "\n");
 		System.out.println();
 		System.out.print(x + "\n");
